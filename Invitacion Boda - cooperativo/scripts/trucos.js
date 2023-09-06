c839d3b32851253bbaf806be5000f0cf170bfe85
Rodrigo-Hernandez <127372674+Rodrigo-Hernandez@users.noreply.github.com>
2023-09-05 23:40:36 -0600

    Primera prueba

---
+++ Invitacion Boda - cooperativo/scripts/trucos.js
@@ -0,0 +1,24 @@
+function updateCountdown() {
+  const now = new Date().getTime();
+  const targetDate = new Date("2023-11-18T16:00:00").getTime();
+  const timeRemaining = targetDate - now;
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = formatTime(days);
+  document.getElementById("hours").textContent = formatTime(hours);
+  document.getElementById("minutes").textContent = formatTime(minutes);
+  document.getElementById("seconds").textContent = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
